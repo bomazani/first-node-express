02d5e9ceab9b8f6783458a1dc5b84a5f333fcd7b
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-16 18:49:09 -0400

    Created two separat paths. One returns a string, the other returns a json object.

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ app.get('/',(req,res,next) => {
     res.send('Hello!!!')
 })
 
+app.get('/json',(req,res,next) => {
+    const data = {
+        greeting: 'Hello!!'
+    }
+    res.json(data)
+})
+
 app.listen(3000)
